71c33cfdec1f21f01587fd8516baea9486ba1e31
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-11-04 15:43:33 -0400

    added  error cond for signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -6,20 +6,29 @@ import { ADD_USER } from '../utils/mutations';
 
 function Signup(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
-  const [addUser] = useMutation(ADD_USER);
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    const mutationResponse = await addUser({
-      variables: {
-        email: formState.email,
-        password: formState.password,
-        firstName: formState.firstName,
-        lastName: formState.lastName,
-      },
-    });
-    const token = mutationResponse.data.addUser.token;
-    Auth.login(token);
+
+    if (error) {
+      console.log(Object.values(error));
+    }
+
+    try {
+      const mutationResponse = await addUser({
+        variables: {
+          email: formState.email,
+          password: formState.password,
+          firstName: formState.firstName,
+          lastName: formState.lastName,
+        },
+      });
+      const token = mutationResponse.data.addUser.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(error);
+    }
   };
 
   const handleChange = (event) => {
